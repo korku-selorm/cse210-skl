5077b4a8771aab31d203c22b390309f362a9b0e9
korku-selorm <korkuselorm01@hmail.com>
2025-09-06 22:15:43 +0000

    Completed excercise 2 conditions

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,57 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("Enter your grade percentage: ");
+        string input = Console.ReadLine();
+
+        //convert the input into an integer from a string
+
+        int gradePercentage = int.Parse(input);
+
+        //String VAriables to store the letter grade
+        string letter = "";
+
+        //determining the letter garde using if else statements
+        if (gradePercentage >= 90)
+        {
+            letter = "A";
+        }
+
+        else if (gradePercentage >= 80)
+        {
+            letter = "B";
+        }
+
+
+        else if (gradePercentage >= 70)
+        {
+            letter = "C";
+        }
+
+
+        else if (gradePercentage >= 60)
+        {
+            letter = "D";
+        }
+
+        else
+        {
+            letter = "F";
+        }
+
+        //Displaying the letter grade
+        Console.WriteLine($"Your grade is: {letter}");
+
+        //check if pass or fail
+
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congratulations! You have passed th course");
+        }
+
+        else
+        {
+            Console.WriteLine("You did not pass. Better luck next time.");
+        }
     }
 }
